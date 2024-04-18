a16ddad39fc0a954cd7cd507f8a5369b3707e70d
Isabela Castro <isabeladpcastro@gmail.com>
2024-04-17 23:04:22 -0300

    first commit

---
+++ Media_C.c
@@ -0,0 +1,64 @@
+/******************************************************************************
+Autor: Isabela de Paula Castro, 824145
+Data de Criação: 08/04/2024
+Data de Modificação: 17/04/2024
+Objetivo: Calcular a media de CAP
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    //Declarações
+    double Prova1;
+    double Prova2;
+    double Prova3;
+    
+    double atv1;
+    double atv2;
+    double atv3;
+    double atv4;
+    double atv5;
+    double atv6;
+    double atv7;
+    double atv8;
+    double atv9;
+    double atv10;
+    
+    double media;
+    
+    //Entradas de dados do usuario
+    printf("Digite a nota da prova 1:");
+    scanf("%lf", &Prova1);
+    printf("Digite a nota da prova 2:");
+    scanf("%lf", &Prova2);
+    printf("Digite a nota da prova 3:");
+    scanf("%lf", &Prova3);
+    
+    printf("Digite a nota da atividade 1:");
+    scanf("%lf", &atv1);
+    printf("Digite a nota da atividade 2:");
+    scanf("%lf", &atv2);
+    printf("Digite a nota da atividade 3:");
+    scanf("%lf", &atv3);
+    printf("Digite a nota da atividade 4:");
+    scanf("%lf", &atv4);
+    printf("Digite a nota da atividade 5:");
+    scanf("%lf", &atv5);
+    printf("Digite a nota da atividade 6:");
+    scanf("%lf", &atv6);
+    printf("Digite a nota da atividade 7:");
+    scanf("%lf", &atv7);
+    printf("Digite a nota da atividade 8:");
+    scanf("%lf", &atv8);
+    printf("Digite a nota da atividade 9:");
+    scanf("%lf", &atv9);
+    printf("Digite a nota da atividade 10:");
+    scanf("%lf", &atv10);
+    
+    //Calcular média da Materia CAP
+    media=(Prova1*0.25)+(Prova2*0.25)+(Prova3*0.3)+(((atv1+atv2+atv3+atv4+atv5+atv6+atv7+atv8+atv9+atv10)/10)*0.2);
+    
+    //Resultado
+    printf("Sua media é %lf", media);
+    return 0;
+}
---
+++ Media_Python.py
@@ -0,0 +1,65 @@
+'''
+Autor: Isabela de Paula Castro, 824145
+Data de Criação: 08/04/2024
+Data de Modificação: 17/04/2024
+Objetivo: Calcular a media de CAP
+'''
+
+#Entrada de Dados
+print("Digite sua nota da Prova 1:")
+prova1 = input()
+prova1 = float(prova1)
+
+print("Digite sua nota da Prova 2:")
+prova2 = input()
+prova2 = float(prova2)
+
+print("Digite sua nota da Prova 3:")
+prova3 = input()
+prova3 = float(prova3)
+
+print("Digite sua nota da Atividade 1:")
+atv1 = input()
+atv1 = float(atv1)
+
+print("Digite sua nota da Atividade 2:")
+atv2 = input()
+atv2 = float(atv2)
+
+print("Digite sua nota da Atividade 3:")
+atv3 = input()
+atv3 = float(atv3)
+
+print("Digite sua nota da Atividade 4:")
+atv4 = input()
+atv4 = float(atv4)
+
+print("Digite sua nota da Atividade 5:")
+atv5 = input()
+atv5 = float(atv5)
+
+print("Digite sua nota da Atividade 6:")
+atv6 = input()
+atv6 = float(atv6)
+
+print("Digite sua nota da Atividade 7:")
+atv7 = input()
+atv7 = float(atv7)
+
+print("Digite sua nota da Atividade 8:")
+atv8 = input()
+atv8 = float(atv8)
+
+print("Digite sua nota da Atividade 9:")
+atv9 = input()
+atv9 = float(atv9)
+
+print("Digite sua nota da Atividade 10:")
+atv10 = input()
+atv10 = float(atv10)
+
+#Calculo da Media de CAP
+media = (prova1*0.25)+(prova2*0.25)+(prova3*0.3)+(((atv1+atv2+atv3+atv4+atv5+atv6+atv7+atv8+atv9+atv10)/10)*0.2)
+
+#Resultado
+print("Media = ", media)
